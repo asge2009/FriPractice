c6a9e44bcf12d4a5ddfb9c52c6d6437d2bf4b6b4
asge2009 <82427068+asge2009@users.noreply.github.com>
2021-04-16 23:00:29 -0400

    edited

--- project1/src/module-info.java
+++ project1/src/module-info.java
@@ -1,2 +1,10 @@
 module project1 {
+	String name = "Jamie Foxx";
+	double Salary = 2000;
+	double bonus = 100;
+	
+	System.out.println("Name of the Employee: " + name);
+	System.out.println("Salary of the Employee: " + Salary);
+	System.out.println("Bonus of the Employee: " + bonus);
+
 }
